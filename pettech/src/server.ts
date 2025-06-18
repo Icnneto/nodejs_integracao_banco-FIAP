17753090c7f09f565faf9c4d9a5ff9b498674771
Israel Nunes <israelnetonunes@gmail.com>
2025-06-17 21:21:47 -0300

    feat: create env variable import with zod

--- pettech/src/server.ts
+++ pettech/src/server.ts
@@ -1,11 +1,12 @@
 /**
  * Responsible for exposing our web server configs
  */
+import { env } from './env';
 import { app } from './app';
 
 app.listen({
     host: '0.0.0.0',
-    port: 3000
+    port: env.PORT
 }).then(() => {
     console.log(`Server running on http://localhost:3000`)
 });
